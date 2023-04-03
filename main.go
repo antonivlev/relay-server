64482b3c2624a9d54e17bb065d1ad661b4bbe829
Anton Ivlev <anton.ivlev@boltzbit.com>
2023-04-03 09:51:16 +0100

    Add checking if user logged in to requests

--- main.go
+++ main.go
@@ -30,16 +30,32 @@ func main() {
 	fmt.Print("done\n")
 
 	users.DB = mySqlDb
+	DB = mySqlDb
 
 	fmt.Println("server running")
 	errServe := http.ListenAndServe(":8080", http.HandlerFunc(handlerOfAllRequests))
 	log.Fatal(errServe)
 }
 
+var DB *sql.DB
+
 func handlerOfAllRequests(w http.ResponseWriter, r *http.Request) {
 	fmt.Println(time.Now().Format("2006-01-02 15:04:05"), r.Method, r.RequestURI)
 
-	routeHandler := routeToHandler[r.Method+" "+r.RequestURI]
+	route := r.Method + " " + r.RequestURI
+
+	publicRouteHandler := publicRouteToHandler[route]
+	if publicRouteHandler != nil {
+		publicRouteHandler(w, r)
+		return
+	}
+
+	if !isUserAuthorized(r) {
+		w.WriteHeader(http.StatusUnauthorized)
+		return
+	}
+
+	routeHandler := routeToHandler[route]
 	if routeHandler != nil {
 		routeHandler(w, r)
 		return
@@ -48,9 +64,22 @@ func handlerOfAllRequests(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusNotFound)
 }
 
+var publicRouteToHandler = map[string]func(http.ResponseWriter, *http.Request){
+	"POST /login": users.PostLogin,
+}
+
 var routeToHandler = map[string]func(http.ResponseWriter, *http.Request){
 	"POST /login": users.PostLogin,
 	"GET /users":  users.GetUsers,
 	"POST /users": users.PostUsers,
 	"POST /api/*": api.PostApi,
+}
+
+func isUserAuthorized(r *http.Request) bool {
+	email, password, _ := r.BasicAuth()
+	matchedUserRow := DB.QueryRow("SELECT id FROM users WHERE email = ? AND password = ?;", email, password)
+	var matchedUserId int
+	errScan := matchedUserRow.Scan(&matchedUserId)
+
+	return errScan == nil
 }
